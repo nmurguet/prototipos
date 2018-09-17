7c901c46d6e76abffb11f6e904fe25605849f4bc
Nicolas <nefisto@gmail.com>
2018-09-17 00:40:40 -0300

    muchas cosas
    sarasa

---
+++ gatherer/gatherer/Assets/scripts/MoveCharacter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour {
+
+	public Transform target; 
+
+	public Rigidbody2D rb; 
+
+	public bool move; 
+
+	public Vector2 velocity; 
+
+	public float speed; 
+	// Use this for initialization
+	void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (move) {
+			transform.position = Vector2.MoveTowards (transform.position, target.position, speed * Time.deltaTime);
+
+		}
+
+		
+	}
+}
