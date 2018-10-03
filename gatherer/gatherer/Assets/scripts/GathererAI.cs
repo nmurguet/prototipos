b9059b767be7bc5c95e1e748966599bd1aa48d31
Nicolas <nefisto@gmail.com>
2018-10-03 01:11:07 -0300

    first

---
+++ gatherer/gatherer/Assets/scripts/GathererAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GathererAI : MonoBehaviour {
+
+	[SerializeField] private Transform goldNodeTransform; 
+	[SerializeField] private Transform storageTransform; 
+
+
+	private IUnit unit; 
+
+	private void Awake()
+	{
+
+		unit = gameObject.GetComponent<IUnit> (); 
+
+		unit.MoveTo (goldNodeTransform.position, 10f, () => {
+			unit.PlayAnimationMine (goldNodeTransform.position, () => {
+				unit.MoveTo (storageTransform.position, 5f, null);
+			});
+		
+		});
+
+	}
+
+			}
+
+
---
+++ gatherer/gatherer/Assets/scripts/IUnit.cs
@@ -0,0 +1,12 @@
+﻿using System; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUnit {
+
+	bool isIdle(); 
+	void MoveTo(Vector3 position, float stopDistance, Action onArriveAtPosition);
+	void PlayAnimationMine(Vector3 lookAtPosition, Action onAnimationCompleted); 
+	
+}
