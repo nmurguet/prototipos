7eb4318ed3e255ad7edf2fcbace29020c9a13fd0
Nicolas Murguet <nefisto@gmail.com>
2018-12-05 09:57:42 -0300

    asd

---
+++ conejito/Assets/scripts/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpForce; 
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(1f * speed, rb.velocity.y);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 1f * jumpForce);
+
+
+        }
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameObject.transform.position = new Vector3(-4, 1, 0);
+    }
+}
