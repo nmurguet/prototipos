fbe76cfc0443aff922bde7d74683bd6930294375
Nicolas Murguet <nefisto@gmail.com>
2019-02-08 12:30:09 -0300

    aa

---
+++ conejito/Assets/scripts/PlatformManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    public GameObject firstPlatform;
+    public GameObject secondPlatform;
+    public GameObject thirdPlatform;
+
+    public GameObject currentPlatform;
+    public GameObject startingPlatform;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Platform")
+        {
+            
+            currentPlatform = collision.gameObject.transform.parent.gameObject;
+            if(currentPlatform.gameObject.name == firstPlatform.gameObject.name)
+            {
+                thirdPlatform.gameObject.transform.position = new Vector3(secondPlatform.gameObject.transform.position.x + 75.861f, 0, 0);
+
+            }
+
+            if (currentPlatform.gameObject.name == thirdPlatform.gameObject.name)
+            {
+                
+                firstPlatform.gameObject.transform.position = new Vector3(currentPlatform.gameObject.transform.position.x + 75.861f,0,0);
+                secondPlatform.gameObject.transform.position = new Vector3(firstPlatform.gameObject.transform.position.x + 75.861f, 0, 0);
+                
+            }
+            
+
+        }
+
+    }
+}
--- conejito/Assets/scripts/player.cs
+++ conejito/Assets/scripts/player.cs
@@ -6,30 +6,81 @@ public class player : MonoBehaviour {
 
     public Rigidbody2D rb;
     public float speed;
-    public float jumpForce; 
+    public float jumpForce;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
+
+    //ground check stuff
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private Animator animator;
+
+
+    private int extraJumps;
+    public int extraJumpsValue;
+    public bool jumpPressed;
+    private bool doublejump;
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        doublejump = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         rb.velocity = new Vector2(1f * speed, rb.velocity.y);
 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 1f * jumpForce);
+
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            extraJumps--;
+            doublejump = true;
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
 
         }
 
+        if (isGrounded)
+        {
+            extraJumps = extraJumpsValue;
+            doublejump = false;
+
+        }
+
+
+
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        animator.SetBool("isJumping", isGrounded);
 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        gameObject.transform.position = new Vector3(-4, 1, 0);
+         //gameObject.transform.position = new Vector3(-4, 1, 0);
+        
+
     }
 }
