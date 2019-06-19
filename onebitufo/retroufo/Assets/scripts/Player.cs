8f4e801efeb24211c81b1015ee1238e5c2db5e1f
Nicolas Murguet <nefisto@gmail.com>
2019-06-19 09:07:30 -0300

    movement

---
+++ onebitufo/retroufo/Assets/scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float torque;
+    public float thrust;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        
+    }
+
+    void Movement()
+    {
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, 18f);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(0.3f * torque);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(-0.3f * torque);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(Vector2.up * thrust*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddRelativeForce(Vector2.up * (thrust / 1.85f) Time.deltaTime);
+        }
+
+        
+
+  
+    }
+}
