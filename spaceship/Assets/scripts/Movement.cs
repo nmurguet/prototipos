3726e3fc67e12ad5e7a21ff9d0588f9821a08486
Nicolas Murguet <nefisto@gmail.com>
2019-09-02 19:07:48 -0300

    space park broke
    New project, finish it

---
+++ spaceship/Assets/scripts/Movement.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    public float turnForce;
+    public float thrustForce;
+    public float strafeForce;
+
+
+    public bool rforceApplied;
+    public bool forceApplied;
+    public bool leftturnApplied;
+    public bool rightturnApplied;
+    public bool leftStrafeApplied;
+    public bool rightStrafeApplied;
+
+    public GameObject bulletPrefab;
+    public GameObject barrel;
+
+    public float bulletSpeed = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        leftturnApplied = false;
+        rightturnApplied = false;
+        rforceApplied = false;
+        forceApplied = false;
+        leftStrafeApplied = false;
+        rightStrafeApplied = false; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftturnApplied = false;
+        rightturnApplied = false;
+        rforceApplied = false;
+        forceApplied = false;
+        leftStrafeApplied = false;
+        rightStrafeApplied = false;
+        if (Input.GetKey(KeyCode.A))
+        {
+            leftturnApplied = true;
+
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rightturnApplied = true;
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            rforceApplied = true;
+
+
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            forceApplied = true; 
+
+
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+
+            leftStrafeApplied = true; 
+
+
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            rightStrafeApplied = true; 
+
+
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            shootBullet();
+
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        Move();
+    }
+
+
+    void Move()
+    {
+        if (leftturnApplied)
+        {
+            rb.AddTorque(0.3f * turnForce * Time.deltaTime);
+
+
+        }
+        if (rightturnApplied)
+        {
+            rb.AddTorque(-0.3f * turnForce * Time.deltaTime);
+        }
+
+
+        if (rforceApplied)
+        {
+
+            rb.AddRelativeForce(Vector2.up * thrustForce * Time.deltaTime);
+
+
+        }
+        if (forceApplied)
+        {
+
+            rb.AddRelativeForce(Vector2.up * -thrustForce * Time.deltaTime);
+
+
+        }
+
+        if (leftStrafeApplied)
+        {
+
+            rb.AddRelativeForce(Vector2.right * -strafeForce * Time.deltaTime);
+
+
+        }
+        if (rightStrafeApplied)
+        {
+
+            rb.AddRelativeForce(Vector2.right * strafeForce * Time.deltaTime);
+
+
+        }
+
+
+
+
+
+    }
+
+    public void shootBullet()
+    {
+        GameObject b = Instantiate(bulletPrefab) as GameObject;
+        b.transform.position = barrel.transform.position;
+        b.transform.rotation = barrel.transform.rotation;
+        Vector2 shootPos = new Vector2(this.transform.position.x - barrel.transform.position.x, this.transform.position.y - barrel.transform.position.y);
+        b.GetComponent<Rigidbody2D>().AddForce(shootPos* -bulletSpeed);
+
+
+
+
+    }
+}
---
+++ spaceship/Assets/scripts/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Rigidbody2D rb;
+
+
+    public float iTimer = 3f;
+    public float timer = 0f; 
+
+    private Vector2 screenBounds; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector2(speed, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+
+
+        if (timer > iTimer)
+            {
+            Destroy(gameObject);
+
+        }
+        
+    }
+}
