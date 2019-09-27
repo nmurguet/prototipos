92f7a9ef670c204db7acdcdd6c1b211b09155089
Nicolas Murguet <nefisto@gmail.com>
2019-09-27 14:30:47 -0300

    Changed background
    added stars,
    planets
    and more asteroids

---
+++ spaceship/Assets/scripts/AutoAim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoAim : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smooth;
+    private float zVelocity = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target) { 
+        Vector3 dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        float z1 = Mathf.SmoothDampAngle(transform.eulerAngles.z, angle + 90f, ref zVelocity, smooth);
+        transform.rotation = Quaternion.Euler(0f, 0f, z1);
+        }
+    }
+}
---
+++ spaceship/Assets/scripts/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public Transform theCamera;
+    public float ParallaxFactor = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+
+        Vector3 newPos = theCamera.position * ParallaxFactor;                // Calculate the position of the object
+        newPos.z = 0;                       // Force Z-axis to zero, since we're in 2D
+        transform.position =  newPos;
+
+    }
+}
